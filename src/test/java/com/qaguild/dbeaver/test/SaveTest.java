f846d519bbb7bb97c0d1bcffdc1ca48ac286b033
SergeyPirogov <semen4ik20@gmail.com>
2020-07-09 18:29:51 +0300

    add: update for save processor

--- src/test/java/com/qaguild/dbeaver/test/SaveTest.java
+++ src/test/java/com/qaguild/dbeaver/test/SaveTest.java
@@ -22,19 +22,28 @@ public class SaveTest extends BaseTest {
         assertThat(newUser.getName()).isEqualTo(user.getName());
     }
 
-    //TODO implement update
-//    @Test
-//    void testCanUpdateEntity() {
-//        User user = new User();
-//        user.setId(3);
-//        user.setName("Kolya");
-//
-//        Integer value = userRepo.save(user);
-//
-//        assertThat(value).isEqualTo(1);
-//
-//        User newUser = userRepo.findOne(3);
-//
-//        assertThat(newUser.getName()).isEqualTo(user.getName());
-//    }
+    @Test
+    void testCanUpdateEntity() {
+        User user = new User();
+        user.setId(3);
+        user.setName("Petya");
+
+        Integer value = userRepo.save(user);
+
+        User oldUser = userRepo.findOne(3);
+
+        assertThat(value).isEqualTo(1);
+        assertThat(oldUser.getName()).isEqualTo(user.getName());
+
+        User newUser = userRepo.findOne(3);
+        newUser.setName("Mykola");
+
+        value = userRepo.save(newUser);
+
+        User updatedUser = userRepo.findOne(3);
+
+        assertThat(value).isEqualTo(1);
+        assertThat(updatedUser.getName()).isEqualTo(newUser.getName());
+
+    }
 }
